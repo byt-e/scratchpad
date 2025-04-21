91a3fe07ae8fea171ecc7c2315e9a64d3827768e
Byte <202784213+byt-e@users.noreply.github.com>
2025-04-21 10:25:38 +0100

    Convert temp.c to use functions

--- c/kandr/00-chapter1/temp.c
+++ c/kandr/00-chapter1/temp.c
@@ -4,12 +4,14 @@
 #define UPPER 300
 #define STEP  20
 
+float fahr_to_celsius(float);
+float celsius_to_fahr(float);
+
 int main() {
     printf("%10s\t%10s\n", "Fahrenheit", "Celsius");
     float fahr;
     for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
-        float celsius = 5 * (fahr-32) / 9;
-        printf("%10.0f\t%10.1f\n", fahr, celsius);
+        printf("%10.0f\t%10.1f\n", fahr, fahr_to_celsius(fahr));
     }
 
     printf("%10s\t%10s\n", "-----", "-----");
@@ -17,9 +19,11 @@ int main() {
     printf("%10s\t%10s\n", "Celsius", "Fahrenheit");
     float celsius;
     for (celsius = LOWER; celsius <= UPPER; celsius += STEP) {
-        fahr = (celsius * 9/5) + 32;
-        printf("%10.0f\t%10.1f\n", celsius, fahr);
+        printf("%10.0f\t%10.1f\n", celsius, celsius_to_fahr(celsius));
     }
 
     return 0;
 }
+
+float fahr_to_celsius(float fahr)    { return 5 * (fahr-32) / 9; }
+float celsius_to_fahr(float celsius) { return (celsius * 9/5) + 32; }
